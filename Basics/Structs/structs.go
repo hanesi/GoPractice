4241dd25135d095cb433c86a38f72dbd98fd0b26
hanesi <ian_hanes@utexas.edu>
2020-02-29 07:51:13 -0500

    Update structs.go

--- Basics/Structs/structs.go
+++ Basics/Structs/structs.go
@@ -7,15 +7,23 @@ type person struct {
 	last_name  string
 }
 
+type secretAgent struct {
+	person
+	boo bool
+}
+
 func main() {
-	p1 := person{
-		first_name: "Ian",
-		last_name:  "Hanes",
+	sa1 := secretAgent{
+		person: person{
+			first_name: "Ian",
+			last_name:  "Hanes",
+		},
+		boo: true,
 	}
 	p2 := person{
 		first_name: "John",
 		last_name:  "Doe",
 	}
-	fmt.Println(p1.first_name, p2.first_name)
-	fmt.Println(p1.last_name, p2.last_name)
+	fmt.Println(sa1.first_name, sa1.last_name, sa1.boo)
+	fmt.Println(p2.first_name, p2.last_name)
 }
