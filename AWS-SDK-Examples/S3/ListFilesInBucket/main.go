3fe59f9519cabe14b9ac75c929c1b7266da1e41a
Ian Hanes <ian@sharelocalmedia.com>
2020-03-02 08:50:13 -0500

    Create main.go

---
+++ AWS-SDK-Examples/S3/ListFilesInBucket/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+
+	"fmt"
+)
+
+func main() {
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+
+	svc := s3.New(sess)
+
+	i := 0
+	bucket := "some-bucket-name" // bucket name here
+	err := svc.ListObjectsPages(&s3.ListObjectsInput{
+		Bucket: &bucket,
+	}, func(p *s3.ListObjectsOutput, last bool) (shouldContinue bool) {
+		fmt.Println("Page,", i)
+		i++
+
+		for _, obj := range p.Contents {
+			fmt.Println("Object:", *obj.Key)
+		}
+		return true
+	})
+	if err != nil {
+		fmt.Println("failed to list objects", err)
+		return
+	}
+
+}
