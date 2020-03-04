402ef63545715ee684d4dbb7117a70d0c0411139
Ian Hanes <ian@sharelocalmedia.com>
2020-03-04 09:06:33 -0500

    Create main.go

---
+++ AWS-SDK-Examples/DatabaseStuff/MySQL/Select/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func main() {
+
+	host, _ := os.LookupEnv("host")
+	port := 3306
+	user, _ := os.LookupEnv("user")
+	password, _ := os.LookupEnv("password")
+	dbname, _ := os.LookupEnv("dbname")
+
+	mysqlInfo := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
+		user, password, host, port, dbname)
+
+	db, err := sql.Open("mysql", mysqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	sqlStatement := "select * from StreamingLog limit 10"
+
+	rows, err := db.Query(sqlStatement)
+	if err != nil {
+		fmt.Println("Failed to run query", err)
+		return
+	}
+
+	cols, err := rows.Columns()
+	if err != nil {
+		fmt.Println("Failed to get columns", err)
+		return
+	}
+	// Result is your slice string.
+	rawResult := make([][]byte, len(cols))
+	result := make([]string, len(cols))
+
+	dest := make([]interface{}, len(cols)) // A temporary interface{} slice
+	for i, _ := range rawResult {
+		dest[i] = &rawResult[i] // Put pointers to each string in the interface slice
+	}
+
+	for rows.Next() {
+		err = rows.Scan(dest...)
+		if err != nil {
+			fmt.Println("Failed to scan row", err)
+			return
+		}
+
+		for i, raw := range rawResult {
+			if raw == nil {
+				result[i] = "\\N"
+			} else {
+				result[i] = string(raw)
+			}
+		}
+
+		fmt.Printf("%#v\n", result)
+	}
+
+}
