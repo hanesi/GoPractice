bec013dfe1a60e588d2fe984481abec5ccb71faf
Ian Hanes <ian@sharelocalmedia.com>
2020-03-24 14:28:56 -0400

    Update main.go

--- Basics/EnvironmentVariables/main.go
+++ Basics/EnvironmentVariables/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strings"
+
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
 
 func main() {
 	// Store the PATH environment variable in a variable
@@ -14,7 +24,9 @@ func main() {
 		// Print the value of the environment variable
 		fmt.Print(path)
 	}
+
 	fmt.Println()
+	fmt.Println(os.LookupEnv("ATHENADB"))
 	for _, e := range os.Environ() {
 		pair := strings.SplitN(e, "=", 2)
 		fmt.Println(pair)
