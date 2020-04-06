49d6889842c1141a2f57c655700d5bfea6129a1c
Ian Hanes <ian@sharelocalmedia.com>
2020-04-06 09:59:43 -0400

    Create main.go

---
+++ Leetcode30DayChallenge/Day6/main.go
@@ -0,0 +1,61 @@
+/*
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+
+Note:
+All inputs will be in lowercase.
+The order of your output does not matter.
+*/
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type sortRunes []rune
+
+func main() {
+	strs := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
+	fmt.Println(groupAnagrams(strs))
+}
+
+func groupAnagrams(strs []string) [][]string {
+	mapper := make(map[string][]string)
+	retSlice := [][]string{}
+	for _, v := range strs {
+		mapper[sortString(v)] = append(mapper[sortString(v)], v)
+	}
+
+	for _, v := range mapper {
+		retSlice = append(retSlice, v)
+	}
+	return retSlice
+}
+
+func (s sortRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sortRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sortRunes) Len() int {
+	return len(s)
+}
+
+func sortString(s string) string {
+	r := []rune(s)
+	sort.Sort(sortRunes(r))
+	return string(r)
+}
