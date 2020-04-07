3612b7f2491f4c97e61c8616caaa82a49a508f6a
Ian Hanes <ian@sharelocalmedia.com>
2020-04-07 08:26:26 -0400

    Update main.go

--- KafkaStockStreaming/AlphaVantage/main.go
+++ KafkaStockStreaming/AlphaVantage/main.go
@@ -1,31 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
+
+type StockData struct {
+	GlobalQuote TickerData `json:"Global Quote"`
+}
+
+type TickerData struct {
+	Symbol           string `json:"01. symbol"`
+	Open             string `json:"02. open"`
+	High             string `json:"03. high"`
+	Low              string `json:"04. low"`
+	Price            string `json:"05. price"`
+	Volume           string `json:"06. volume"`
+	LatestTradingDay string `json:"07. latest trading day"`
+	PreviousClose    string `json:"08. previous close"`
+	Change           string `json:"09. change"`
+	ChangePercent    string `json:"10. change percent"`
+}
 
 func init() {
 	// loads values from .env into the system
 	if err := godotenv.Load(); err != nil {
 		log.Print("No .env file found")
 	}
+	return
 }
 
 func main() {
 	// Store the PATH environment variable in a variable
-	AVkey, exists := os.LookupEnv("AVkey")
+	AVkey, _ := os.LookupEnv("AVkey")
 
-	if exists {
-		// Print the value of the environment variable
-		fmt.Print(AVkey)
-	}
-	AVkey = "OL54MJQGMZ7DX0N0"
 	url := (buildQueryURL("GOOG", AVkey))
 
 	resp, err := http.Get(url)
@@ -37,7 +52,16 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("%s", body)
+	bodyString := fmt.Sprintf("%s", body)
+
+	var data StockData
+	decoder := json.NewDecoder(strings.NewReader(bodyString))
+	err = decoder.Decode(&data)
+	if err != nil {
+		fmt.Println("twas an error")
+		// return
+	}
+	fmt.Println(data)
 }
 
 func buildQueryURL(s, av string) string {
