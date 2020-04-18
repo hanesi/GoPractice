581510f61952f7023fe364945bdf4c689279c04e
Ian Hanes <ian@sharelocalmedia.com>
2020-04-18 09:42:26 -0400

    Create main.go

---
+++ Leetcode30DayChallenge/Day18/main.go
@@ -0,0 +1,50 @@
+/*
+Given a m x n grid filled with non-negative numbers,
+find a path from top left to bottom right which
+minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+Example:
+Input:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+Output: 7
+Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	grid := [][]int{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}}
+	fmt.Println(minPathSum(grid))
+}
+
+func minPathSum(grid [][]int) int {
+	var m int = len(grid)
+	var n int = len(grid[0])
+	for i := 1; i < m; i++ {
+		grid[i][0] += grid[i-1][0]
+	}
+	for j := 1; j < n; j++ {
+		grid[0][j] += grid[0][j-1]
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+		}
+	}
+	return grid[m-1][n-1]
+
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
