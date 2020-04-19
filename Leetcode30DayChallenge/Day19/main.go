1a750f7cd02bcffc9982de78cce26cf983395302
Ian Hanes <ian@sharelocalmedia.com>
2020-04-19 08:50:20 -0400

    Update main.go

--- Leetcode30DayChallenge/Day19/main.go
+++ Leetcode30DayChallenge/Day19/main.go
@@ -28,12 +28,26 @@ func main() {
 }
 
 func search(nums []int, target int) int {
-	numMap := make(map[int]int)
-	for i, v := range nums {
-		numMap[v] = i
-	}
-	if val, ok := numMap[target]; ok {
-		return val
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] <= nums[right] {
+			if target <= nums[right] && target >= nums[mid] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		} else {
+			if target >= nums[left] && target <= nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		}
 	}
 	return -1
 }
