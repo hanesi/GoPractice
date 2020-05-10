d35301c01e296008cde284334d11db5cdf1b0714
hanesi <ian_hanes@utexas.edu>
2020-05-09 22:09:26 -0400

    Update main.go

--- DataframeStuff/NFL_Data/main.go
+++ DataframeStuff/NFL_Data/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+
+	// Need to install this and gonum.org/v1/gonum, but no import required for gonum
+	"github.com/go-gota/gota/dataframe"
+	"github.com/go-gota/gota/series"
 )
 
 func main() {
@@ -12,6 +16,25 @@ func main() {
 		fmt.Println(err)
 	}
 
-	df := qframe(csv)
+	df := dataframe.ReadCSV(csv)
+
+	homeSeries := df.Col("home_score")
+	awaySeries := df.Col("away_score")
+
+	homeWin := homeWin(homeSeries, awaySeries)
+	df = df.Mutate(homeWin)
+
 	fmt.Println(df)
+}
+
+func homeWin(hs, as series.Series) series.Series {
+	homeWSlice := []bool{}
+	for i := 0; i < hs.Len(); i++ {
+		if hs.Val(i).(int) > as.Val(i).(int) {
+			homeWSlice = append(homeWSlice, true)
+		} else {
+			homeWSlice = append(homeWSlice, false)
+		}
+	}
+	return series.New(homeWSlice, series.Bool, "homeWinSeries")
 }
