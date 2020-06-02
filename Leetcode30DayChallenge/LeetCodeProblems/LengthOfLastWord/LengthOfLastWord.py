cf4f80ea7bd8c978d3032b8533c8ebfb46a0d486
Ian Hanes <ian@sharelocalmedia.com>
2020-06-02 08:37:32 -0400

    Create LengthOfLastWord.py

---
+++ Leetcode30DayChallenge/LeetCodeProblems/LengthOfLastWord/LengthOfLastWord.py
@@ -0,0 +1,24 @@
+"""
+Given a string s consists of upper/lower-case alphabets
+and empty space characters ' ', return the length of last
+word (last word means the last appearing word if we loop
+from left to right) in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a maximal substring consisting
+of non-space characters only.
+
+Example:
+Input: "Hello World"
+Output: 5
+"""
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.strip()
+        wordList = s.split(' ')
+        return len(wordList[len(wordList)-1])
+
+tmp = Solution()
+print(lengthOfLastWord("Hello World"))
