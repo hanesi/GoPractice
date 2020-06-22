6531c7753a7560490a3206d4288444d1afdae8c0
Ian Hanes <ian@sharelocalmedia.com>
2020-06-22 08:28:36 -0400

    Create main.go

---
+++ Leetcode30DayChallenge/LeetCodeProblems/RemoveElement/main.go
@@ -0,0 +1,40 @@
+/*
+Given an array nums and a value val, remove
+all instances of that value in-place and
+return the new length.
+
+Do not allocate extra space for another
+array, you must do this by modifying the
+input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It
+doesn't matter what you leave beyond the
+new length.
+
+Example 1:
+Given nums = [3,2,2,3], val = 3,
+
+Your function should return length = 2,
+with the first two elements of nums being 2.
+
+It doesn't matter what you leave beyond the
+returned length.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(removeElement([]int{3, 2, 2, 3}, 3))
+}
+
+func removeElement(nums []int, val int) int {
+	count := 0
+	for i, num := range nums {
+		nums[i-count] = nums[i]
+		if num == val {
+			count += 1
+		}
+	}
+	return len(nums) - count
+}
