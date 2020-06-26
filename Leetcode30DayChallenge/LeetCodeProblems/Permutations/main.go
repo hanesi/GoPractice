4ccea70541c6e4781380b12f3c383601b65177e6
Ian Hanes <ian@sharelocalmedia.com>
2020-06-26 08:38:12 -0400

    Create main.go

---
+++ Leetcode30DayChallenge/LeetCodeProblems/Permutations/main.go
@@ -0,0 +1,51 @@
+/*
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(permute([]int{1, 2, 3}))
+}
+
+func permute(arr []int) [][]int {
+	var helper func([]int, int)
+	res := [][]int{}
+
+	helper = func(arr []int, n int) {
+		if n == 1 {
+			tmp := make([]int, len(arr))
+			copy(tmp, arr)
+			res = append(res, tmp)
+		} else {
+			for i := 0; i < n; i++ {
+				helper(arr, n-1)
+				if n%2 == 1 {
+					tmp := arr[i]
+					arr[i] = arr[n-1]
+					arr[n-1] = tmp
+				} else {
+					tmp := arr[0]
+					arr[0] = arr[n-1]
+					arr[n-1] = tmp
+				}
+			}
+		}
+	}
+	helper(arr, len(arr))
+	return res
+}
