5fe14227c57549eb38f50d3ddc83ad27e1a57faa
Ian Hanes <ian@sharelocalmedia.com>
2020-07-02 10:02:38 -0400

    Create main.go

---
+++ NCOA/GetFile/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func main() {
+	getObject("SLM0915_inputData_preNCOA.csv", "slm-test-bucket-transactional")
+}
+
+func getObject(key, bucket string) {
+	// Initialize an AWS session with configured credentials
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Define client
+	s3Client := s3.New(sess)
+
+	// Using the key, get the object from the bucket
+	obj, err := s3Client.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	print(obj)
+}
